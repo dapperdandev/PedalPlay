ccbb5d81d6f835dfb3e37e41880c168c90b1c531
Daniel Breen <daninreallife7@gmail.com>
2025-05-21 13:00:13 -0600

    First Commit

---
+++ DFCDashboard/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DFCDashboard
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
